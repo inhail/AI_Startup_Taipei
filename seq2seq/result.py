4026970d872fc24646b7d45105729cfc88a861fa
Cecile Liu <ager89@yahoo.com.tw>
2019-07-26 06:27:55 +0800

    Add files via upload

---
+++ seq2seq/result.py
@@ -0,0 +1,162 @@
+
+# Initialized
+# --------------------------------------------------------------------------------
+# Step 0
+# Loss 3.32402
+# Batch Accuracy: 2.23
+# Validation Accuracy: 6.34
+# Input Test String ['f what are re#']
+# Output Prediction['an kn kn kn kn']
+# Actual['f tahw era er#']
+# --------------------------------------------------------------------------------
+# Step 1000
+# Loss 2.192434
+# Batch Accuracy: 33.37
+# Validation Accuracy: 18.51
+# Input Test String ['ilism or anom#']
+# Output Prediction['   e e    e c#']
+# Actual['msili ro mona#']
+# --------------------------------------------------------------------------------
+# Step 2000
+# Loss 2.0783768
+# Batch Accuracy: 37.39
+# Validation Accuracy: 20.32
+# Input Test String ['the state the#']
+# Output Prediction['e e e eeeee ##']
+# Actual['eht etats eht#']
+# --------------------------------------------------------------------------------
+# Step 3000
+# Loss 1.7459737
+# Batch Accuracy: 46.65
+# Validation Accuracy: 21.03
+# Input Test String ['nts that advo#']
+# Output Prediction['e oeeeee eeos#']
+# Actual['stn taht ovda#']
+# --------------------------------------------------------------------------------
+# Step 4000
+# Loss 1.0306189
+# Batch Accuracy: 68.97
+# Validation Accuracy: 41.85
+# Input Test String ['rpretations o#']
+# Output Prediction['srttorrtero t#']
+# Actual['snoitaterpr o#']
+# --------------------------------------------------------------------------------
+# Step 5000
+# Loss 0.6233525
+# Batch Accuracy: 80.13
+# Validation Accuracy: 49.20
+# Input Test String ['ers are unnec#']
+# Output Prediction['ser ena neres#']
+# Actual['sre era cennu#']
+# --------------------------------------------------------------------------------
+# Step 6000
+# Loss 0.35876188
+# Batch Accuracy: 89.40
+# Validation Accuracy: 63.18
+# Input Test String ['s ruler chief#']
+# Output Prediction['s relru eehxe#']
+# Actual['s relur feihc#']
+# --------------------------------------------------------------------------------
+# Step 7000
+# Loss 0.21181549
+# Batch Accuracy: 93.97
+# Validation Accuracy: 72.23
+# Input Test String ['efined anarch#']
+# Output Prediction['denefi ccarec#']
+# Actual['denife hcrana#']
+# --------------------------------------------------------------------------------
+# Step 8000
+# Loss 0.17563097
+# Batch Accuracy: 93.86
+# Validation Accuracy: 81.59
+# Input Test String ['ganization of#']
+# Output Prediction['noitanigan of#']
+# Actual['noitazinag fo#']
+# --------------------------------------------------------------------------------
+# Step 9000
+# Loss 0.10467919
+# Batch Accuracy: 97.32
+# Validation Accuracy: 82.19
+# Input Test String [' pejorative w#']
+# Output Prediction[' evitaropre w#']
+# Actual[' evitarojep w#']
+# --------------------------------------------------------------------------------
+# Step 10000
+# Loss 0.073308945
+# Batch Accuracy: 98.10
+# Validation Accuracy: 89.74
+# Input Test String ['the sans culo#']
+# Output Prediction['eht snal osuo#']
+# Actual['eht snas oluc#']
+# --------------------------------------------------------------------------------
+# Step 11000
+# Loss 0.052505195
+# Batch Accuracy: 98.33
+# Validation Accuracy: 93.36
+# Input Test String ['y working cla#']
+# Output Prediction['y gnikrow alc#']
+# Actual['y gnikrow alc#']
+# --------------------------------------------------------------------------------
+# Step 12000
+# Loss 0.04594732
+# Batch Accuracy: 98.55
+# Validation Accuracy: 95.77
+# Input Test String ['mic instituti#']
+# Output Prediction['cim itutitsni#']
+# Actual['cim itutitsni#']
+# --------------------------------------------------------------------------------
+# Step 13000
+# Loss 0.04312723
+# Batch Accuracy: 98.44
+# Validation Accuracy: 94.67
+# Input Test String ['of what are r#']
+# Output Prediction['fo tahw era r#']
+# Actual['fo tahw era r#']
+# --------------------------------------------------------------------------------
+# Step 14000
+# Loss 0.05179871
+# Batch Accuracy: 98.33
+# Validation Accuracy: 96.68
+# Input Test String ['hilism or ano#']
+# Output Prediction['msilih ro nae#']
+# Actual['msilih ro ona#']
+# --------------------------------------------------------------------------------
+# Step 15000
+# Loss 0.036533587
+# Batch Accuracy: 98.88
+# Validation Accuracy: 96.38
+# Input Test String [' the state th#']
+# Output Prediction[' eht etats ht#']
+# Actual[' eht etats ht#']
+# --------------------------------------------------------------------------------
+# Step 16000
+# Loss 0.032018248
+# Batch Accuracy: 99.44
+# Validation Accuracy: 96.48
+# Input Test String ['ents that adv#']
+# Output Prediction['stne taht vda#']
+# Actual['stne taht vda#']
+# --------------------------------------------------------------------------------
+# Step 17000
+# Loss 0.043570705
+# Batch Accuracy: 99.00
+# Validation Accuracy: 97.79
+# Input Test String ['erpretations #']
+# Output Prediction['snoitaterepe #']
+# Actual['snoitaterpre #']
+# --------------------------------------------------------------------------------
+# Step 18000
+# Loss 0.043942872
+# Batch Accuracy: 98.55
+# Validation Accuracy: 97.38
+# Input Test String ['lers are unne#']
+# Output Prediction['srel era ennu#']
+# Actual['srel era ennu#']
+# --------------------------------------------------------------------------------
+# Step 19000
+# Loss 0.006277927
+# Batch Accuracy: 100.00
+# Validation Accuracy: 99.50
+# Input Test String ['ns ruler chie#']
+# Output Prediction['sn relur eihc#']
+# Actual['sn relur eihc#']
